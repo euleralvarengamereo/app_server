5e2d57d366a5b0a952456946e1655783fcbdab3d
Rithie <euler.sistemas@gmail.com>
2018-02-16 08:36:03 -0200

    Added a aws remote db

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useMongoClient: true });
+mongoose.connect('mongodb://euler:euler@ds011248.mlab.com:11248/mereo-performance', { useMongoClient: true });
+
+// mongodb://<dbuser>:<dbpassword>@ds123658.mlab.com:23658/mereo-performance
+
+// mongoose.connect('mongodb://euler:euler@ds011248.mlab.com:11248/mereo-performance',['app']);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
